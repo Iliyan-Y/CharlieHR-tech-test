04613e9d982358860121d83c0ce8ef3dac7631e5
Iliyan-Y <maximizebg@gmail.com>
2020-11-03 14:55:53 +0000

    GREEN First test added for project set up

---
+++ lib/user.rb
@@ -0,0 +1,11 @@
+require 'date'
+class User 
+
+  attr_reader :name, :date_of_birth
+
+  def initialize(name, date_of_birth)
+    @name = name
+    @date_of_birth = date_of_birth
+  end
+
+end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,12 +1,14 @@
-require "user"
+require 'user'
+
 
 describe User do
   
   it "creates user with name and date of bird" do 
-    date_of_birth = Date.new​(​1986​, ​1​, ​1​)
+    date_of_birth = Date.new(1991,1,1)
     user = User.new("George", date_of_birth)
     expect(user.name).to eq "George"
-    expec(user.date_of_birth).to eq date_of_birth
+    expect(user.date_of_birth).to eq date_of_birth
   end
   
+
 end
