796a3b42f5c27cd182a8f86cf7695fbd1e156212
Iliyan-Y <maximizebg@gmail.com>
2020-11-03 15:14:46 +0000

    GREEN different user's bdays test added

--- lib/user.rb
+++ lib/user.rb
@@ -10,11 +10,11 @@ class User
 
   def age
     current_date = Date.today
-    current_date.year - date_of_birth.year
+    current_date.year - @date_of_birth.year
   end
 
   def next_birthday 
-    "2021-11-01"
+    Date.new(Date.today.year + 1, @date_of_birth.month, @date_of_birth.day).to_s
   end
 
 end
